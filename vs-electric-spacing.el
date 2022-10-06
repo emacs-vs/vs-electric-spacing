886c831625e8726376f051c8f068a5b2724c4ac9
Jen-Chieh Shen <jcs090218@gmail.com>
2022-10-06 20:09:20 +0800

    add src

---
+++ vs-electric-spacing.el
@@ -0,0 +1,37 @@
+;;; vs-electric-spacing.el --- Add spacing around operators like Visual Studio  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Shen, Jen-Chieh
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; URL: https://github.com/jcs-elpa/vs-electric-spacing
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1"))
+;; Keywords: convenience electric vs
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Add spacing around operators like Visual Studio.
+;;
+
+;;; Code:
+
+
+
+(provide 'vs-electric-spacing)
+;;; vs-electric-spacing.el ends here
